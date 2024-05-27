0201a72680cbfc73e1d7fb8e8cc4184beab0b2cc
Tegar Machfudzi <v-tegarmachfudzi@anteraja.id>
2024-05-27 12:39:18 +0700

    add dto

---
+++ src/main/java/com/kafka_demo/demo_kafka/dto/SecretMessageDto.java
@@ -0,0 +1,14 @@
+package com.kafka_demo.demo_kafka.dto;
+
+public class SecretMessageDto {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
